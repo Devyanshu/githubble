96c5b57d507e5b1949226908cc4a335c0165f0a4
Devyanshu <devyanshuhaha@gmail.com>
2019-12-10 11:08:47 +0530

    fixed graph display issue

--- user.py
+++ user.py
@@ -174,9 +174,9 @@ class User:
         self.weekday_wise_contributions = dct
 
     def find_month_wise(self, activity):
-        months = {'01':'January', '02':'February', '03':'March', '04':'April', '05':'May',
-            '06':'June', '07':'July', '08':'August', '09':'September', '10':'October',
-            '11':'November', '12':'December'}
+        months = {'01':'Jan', '02':'Feb', '03':'Mar', '04':'April', '05':'May',
+            '06':'June', '07':'July', '08':'Aug', '09':'Sep', '10':'Oct',
+            '11':'Nov', '12':'Dec'}
 
         dct = {}
         days = list(activity.keys())
