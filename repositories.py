5acb862e70dffaf8227b1d78ed4d73997c2315dd
Devyanshu <devyanshuhaha@gmail.com>
2019-12-11 12:50:08 +0530

    added repository analysis

---
+++ repositories.py
@@ -0,0 +1,54 @@
+from urllib.request import urlopen
+import json
+import tempdata
+
+
+class Repos:
+    def __init__(self, username):
+        self.username = username
+        self.url = 'https://api.github.com/users/' + self.username + '/repos'
+        self.repos = {}
+        self.data = {}
+        self.get_data()
+
+    def get_data(self):
+        r = urlopen(self.url).read()
+        # self.data = json.loads(r.decode('utf-8'))
+        # reading from a cache for testing
+        self.data = tempdata.data
+
+    def get_repos_info(self):
+        for repo in self.data:
+            self.repos[repo['name']] = {
+                'created': repo['created_at'],
+                'isfork': repo['fork'],
+                'language': repo['language'],
+                'stars': repo['stargazers_count'],
+                'watchers': repo['watchers'],
+                'forks': repo['forks']
+            }
+
+    def languages_analysis(self):
+        all_languages = {}
+        non_fork_languages = {}
+        repo_names = list(self.repos.keys())
+        languageUrl = 'https://api.github.com/repos/' + self.username + '/'
+        for i in repo_names:
+            temp = urlopen(languageUrl + i + '/languages').read()
+            lang_data = json.loads(temp.decode('utf-8'))
+            if not self.repos[i]['isfork']:
+                for lang in lang_data:
+                    non_fork_languages[lang] = non_fork_languages.get(
+                        lang, 0) + lang_data[lang]
+            for lang in lang_data:
+                all_languages[lang] = all_languages.get(
+                    lang, 0) + lang_data[lang]
+
+        print(all_languages)
+        print(non_fork_languages)
+
+
+if __name__ == '__main__':
+    test = Repos('Devyanshu')
+    test.get_repos_info()
+    test.languages_analysis()
